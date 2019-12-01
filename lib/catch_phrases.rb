33eb6bb53a4f66e5a0b03ab889a3a8c0219d5190
Ceyvion Biggs <ceyvionb@gmail.com>
2019-12-01 19:15:34 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,6 @@
 def mario
-  phrase = "It's-a me, Mario!"
-  puts phrase
+  status = "It's-a me, Mario!"
+  puts status
 end
 
 def toadstool
@@ -12,4 +12,6 @@ def link
   status = "It's Dangerous To Go Alone! Take This."
   puts  status
 end
+  
+
   
